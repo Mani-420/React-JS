473dd2b1178c768a75bbc81051d7e16c1555740d
Mani-420 <tahirabdulrehman46@gmail.com>
2025-03-25 00:18:22 +0500

    Main

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Name from './Name';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World | This is React Series</h1>
+      <Name />
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Name.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Name() {
+  return <h1>My name is Abdul Rehman Tahir</h1>;
+}
+
+export default Name;
