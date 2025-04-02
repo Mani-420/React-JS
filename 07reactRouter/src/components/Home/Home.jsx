530b5c8eb29124a9d24e43c5f346fff78d8943cf
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-02 21:01:50 +0500

    Home Component

--- 07reactRouter/src/components/Home/Home.jsx
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
                 fill="white"
                 width="24"
                 height="24"
-                xmlns="http://www.w3.org/2000/svg"
+                xmlns="https://bluesoft.com/wp-content/uploads/2024/11/MicrosoftTeams-image.jpg"
                 fillRule="evenodd"
                 clipRule="evenodd"
               >
