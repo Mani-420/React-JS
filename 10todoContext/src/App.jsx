1f50cd70ed719882ba9d0910e0443050f2ec54cc
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-06 21:00:03 +0500

    Main

--- 10todoContext/src/App.jsx
+++ 10todoContext/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { TodoProvider } from './contexts';
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -19,7 +21,7 @@ function App() {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
 
-  const toggleCompleted = (id) => {
+  const toggleComplete = (id) => {
     setTodos((prev) =>
       prev.map((prevTodo) =>
         prevTodo.id === id
@@ -42,15 +44,23 @@ function App() {
 
   return (
     <TodoProvider
-      value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
     >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
-          <div className="flex flex-wrap gap-y-3"></div>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </TodoProvider>
