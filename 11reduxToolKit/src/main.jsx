14744337c18c15f1ad9b69e7c630a38750200be9
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-07 23:58:44 +0500

    Main

--- 11reduxToolKit/src/main.jsx
+++ 11reduxToolKit/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+
+// Redux Toolkit files
+
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
