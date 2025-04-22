6fa8f31439758b67aea5d919024aef5d46217d0b
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-22 19:15:34 +0500

    Add Login, PostCard, and PostForm components to index export

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -5,5 +5,19 @@ import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn';
 import RTE from './RTE';
 import Signup from './Signup';
+import Login from './Login';
+import PostCard from './PostCard';
+import PostForm from './post-form/PostForm';
 
-export { Header, Footer, Logo, Container, LogoutBtn, RTE, Signup };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm
+};
