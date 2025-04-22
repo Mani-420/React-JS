7e46cf0a2fbe7f1e4174bec927ba32c6f5452629
Mani-420 <tahirabdulrehman46@gmail.com>
2025-04-22 18:54:55 +0500

    Add RTE and Signup components to index export

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from './footer/Footer';
 import Container from './container/Container';
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn';
+import RTE from './RTE';
+import Signup from './Signup';
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export { Header, Footer, Logo, Container, LogoutBtn, RTE, Signup };
